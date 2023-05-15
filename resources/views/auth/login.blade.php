5176a1b580d77c5734255ec3b626f5e7a87669e6
santoshkalukunda <santoshkalukunda@gmail.com>
2023-05-15 13:02:36 +0545

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,93 +1,80 @@
 @extends('layouts.app')
-
-@push('styles')
-    <style>
-        #login-form-wrap {
-            display: flex;
-            height: 100vh;
-            align-items: center;
-            justify-content: center;
-        }
-
-        #login-form-wrap>.card {
-            width: 400px;
-        }
-    </style>
-@endpush
-
 @section('content')
-    <div id="login-form-wrap" class="d-block" style="height: 100vh;overflow:hidden">
-        <div style="height: 10%"></div>
-        @if (App\Mysetting::first())
-            <h1 class="h3 font-weight-bold text-uppercase text-center">{{ App\Mysetting::first()->default_province }}
-            </h1>
-            <h2 class="h4 font-weight-bold text-uppercase text-center">{{ App\Mysetting::first()->default_district }}</h2>
-            <h3 class="h5 font-weight-bold text-uppercase text-center">{{ App\Mysetting::first()->default_municipality }}
-            </h3>
-        @endif
-        <div style="height: 10%"></div>
-        <div class="col-12 d-flex justify-content-center">
-            <div class="card font-roboto card-shadow z-depth-0 col-xl-3 col-lg-4 col-md-6">
-                <div class="card-body pt-0 px-4 pb-4">
-                    <div class="col-12 d-flex justify-content-center">
-                        <img class="img-responsive rounded-circle bg-white p-2"
-                            src="{{ asset('assets/image/nep-gov-logo-sm.png') }}" alt=""
-                            style="height: 100px; width: 110px; margin-top: -50px">
+    <body style="background-color: #252627">
+        <div class="container justify-content-center" style="margin-top: 6%;">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="container" style="background-color: #4064aa; border-radius: 25px;">
+                        <div class="row justify-content-center">
+                            <div class="col-md-12 p-4 text-center text-white font-weight-bold">
+                                <h3 class="h4">व्यक्तिगत घटना दर्ता प्रणाली</h3>
+                                <h3 class="h4">Vital Event Registration System</h3>
+                            </div>
+
+                            <div class="col-md-6 py-5" style="background-color:#1d4188; border-radius: 0px 0px 0px 25px;">
+                                <div class="text-center text-white font-weight-bold">
+                                    <div class="d-flex justify-content-center">
+                                        <img class="img-responsive p-2"
+                                            src="{{ asset('assets/image/nep-gov-logo-sm.png') }}" style="height: 100px;">
+                                    </div>
+                                    @php
+                                        $mySetting = App\Mysetting::first();
+                                    @endphp
+                                    @if ($mySetting)
+                                        <h1 class="h4">
+                                            {{ $mySetting->default_municipality }}<br>{{ $mySetting->default_province }},
+                                            <span class="capitalize">{{ $mySetting->default_district }}</span>
+                                        </h1>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 p-5" style="background-color: white; border-radius: 0px 0px 25px 0px;">
+                                <div class="text-center">
+                                    <h6 class="py-3">Login</h6>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <input id="username" type="text"
+                                                class="form-control @error('username') is-invalid @enderror" name="username"
+                                                value="{{ old('username') }}" required autocomplete="username" autofocus
+                                                placeholder="Username">
+
+                                            @error('username')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="mb-3">
+                                            <input id="password" type="password"
+                                                class="form-control @error('password') is-invalid @enderror" name="password"
+                                                required autocomplete="current-password" placeholder="Password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div>
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                        <div class="form-group mt-3">
+                                            <button type="submit" class="btn btn-dark px-4"
+                                                style="background-color:#1d4188">{{ __('Login') }}</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <h2 class="h2-responsive text-center font-weight-bold my-4">{{ __('Login') }}</h2>
-                    <div class="my-5"></div>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <input type="text" id="username" name="username"
-                                class="form-control grey lighten-5 p-4  @error('username') is-invalid @enderror"
-                                placeholder="Username" value="{{ old('username') }}" required autocomplete="username"
-                                autofocus>
-                            @error('username')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <input type="password" id="password" name="password"
-                                class="form-control grey lighten-5 p-4 @error('password') is-invalid @enderror"
-                                placeholder="Password" required autocomplete="current-password">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                    {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
-                            </div>
-                        </div>
-
-                        <div class="form-group text-center">
-
-                            <div class="p-3" style="background-color:#f9fafb">
-                                <button class="btn my-0 rounded z-depth-0 font-16px py-2 px-4 waves-effect waves-light"
-                                    type="submit"
-                                    style="background-color:#374f67; color: #fff;">{{ __('Login') }}</button>
-                            </div>
-
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
+    </body>
 @endsection
